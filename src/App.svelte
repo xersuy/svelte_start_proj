11a5805b46ff2747fbd60e2e6593516b55314dd7
SEOYUL <luyoes20@gmail.com>
2022-04-12 00:51:15 +0900

    init study

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let name;
+  import Header from "./components/header.svelte";
+  import Content from "./components/Content.svelte";
+  import Bottom from "./components/Bottom.svelte";
+</script>
+
+<main>
+  <Header />
+  <h1>Hello {name}!</h1>
+  <Content />
+  <Bottom />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/components/Content.svelte
@@ -0,0 +1,19 @@
+<script>
+  import SubContent from "./SubContent.svelte";
+  let count = 0;
+
+  const handleClick = () => {
+    count++;
+    console.log(count);
+  };
+</script>
+
+<p>Content Space</p>
+<button on:click={handleClick}>
+  클릭수 {count}
+</button>
+<SubContent />
+<SubContent />
+<SubContent />
+<SubContent />
+<SubContent />
